3cc420c04bb7622a108504047ea833ef094197d2
angelabllt <angela.llontop@pucp.edu.pe>
2024-09-19 01:40:17 -0500

    Update Bloqueo.java

--- algoritmos/AlgoritmoGenetico/src/Clases/Bloqueo.java
+++ algoritmos/AlgoritmoGenetico/src/Clases/Bloqueo.java
@@ -1,8 +1,28 @@
 package Clases;
-
-// Nombre: Angela Llontop
-// CódigoPUCP: 20181897 
+import java.time.LocalDateTime;
 
 public class Bloqueo {
+    private String ubigeoOrigen;
+    private String ubigeoDestino;
+    private LocalDateTime fechaHoraInicio;
+    private LocalDateTime fechaHoraFin;
 
+    public Bloqueo(String ubigeoOrigen, String ubigeoDestino, LocalDateTime fechaHoraInicio, 
+            LocalDateTime fechaHoraFin) {
+        this.ubigeoOrigen = ubigeoOrigen;
+        this.ubigeoDestino = ubigeoDestino;
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraFin = fechaHoraFin;
+    }
+
+    @Override
+    public String toString() {
+        return "Bloqueo{" +
+                "ubigeoOrigen='" + ubigeoOrigen + '\'' +
+                ", ubigeoDestino='" + ubigeoDestino + '\'' +
+                ", fechaHoraInicio=" + fechaHoraInicio +
+                ", fechaHoraFin=" + fechaHoraFin +
+                '}';
+    }
 }
+
