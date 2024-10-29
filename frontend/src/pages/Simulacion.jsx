1eef564aa57f191eeeb341e11842127e240144d9
luciaDC <131621597+luciaDC@users.noreply.github.com>
2024-10-29 09:55:28 -0500

    pagina de simulacion - falta colocar leyenda

--- frontend/src/pages/Simulacion.jsx
+++ frontend/src/pages/Simulacion.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { DatePicker, Tabs, Button } from "antd";
-import Tabla from "../components/Tabla";
+import TablaFlota from "../components/TableFlota";
 import MapaPeru from "../components/MapaPeru";
 
 const Simulacion = () => {
@@ -18,83 +18,93 @@ const Simulacion = () => {
 	}, []);
 
 	return (
-		<div className="h-full flex flex-col">
-			
-			<div className="w-full bg-blue-300 p-4 flex justify-center items-center">
+		<div className="h-full flex flex-col relative">
+			{/* Contenedor Superior de hora */}
+			<div className="w-full flex justify-center items-center"  style={{ height: "25px", backgroundColor: '#ffd700' }}>
+
 				<div className="flex justify-between items-center">
 					<h2>Hora</h2>
 					<div className="text-lg">{currentTime}</div>
 				</div>
 			</div>
-			
+		
 			<div className="flex flex-1">
 				{/* Contenedor izquierdo */}
-				<div className="w-1/5 bg-gray-200 p-4">
+				<div className="p-4 overflow-hidden" style={{ height: "490px", width: "200px", backgroundColor: '#fafafa' }}>
 					<div>Leyenda</div>
 				</div>
-
+		
 				{/* Contenedor del mapa */}
-				<div className="w-full h-full">
+				<div className="w-3/5 h-64 overflow-hidden" style={{ height: "490px", width: "700px" }}>
 					<MapaPeru />
 				</div>
 			</div>
-
+		
 			{/* Contenedor inferior con el botón y las pestañas */}
-			<div className="bg-stone-600 p-4 relative">
+			<div
+				className={`bg-gray-200 p-4 absolute bottom-0 w-full transition-transform duration-300 ${
+				isSectionVisible ? "translate-y-80" : "translate-y-full"
+				}`}
+				style={{ zIndex: 10 }}
+			>
 				<Button
-					className="absolute top-2 right-2 z-10"
+					className="absolute top-[-20px] left-1/2 transform -translate-x-1/2 z-20 bg-gray-300 border-gray-300 text-black"
 					onClick={() => setIsSectionVisible(!isSectionVisible)}
+					style={{
+						backgroundColor: '#e5e7eb', // Color gris oscuro
+						borderColor: '#e5e7eb', // Mismo color que el fondo
+					}}
 				>
 					{isSectionVisible ? "Ocultar" : "Mostrar"}
 				</Button>
 
 				{isSectionVisible && (
-					<div className="mt-4">
-						<Tabs
-							defaultActiveKey="1"
-							className="w-full"
-							items={[
-								{
-									label: "Por fecha",
-									key: "1",
-									children: (
-										<>
-											<div className="flex mb-4">
-												<div>
-													Fecha Inicio
-													<DatePicker className="ml-4" placeholder="Seleccionar fecha" />
-												</div>
-												<div className="pl-4">
-													Fecha Fin
-													<DatePicker className="ml-4" placeholder="Seleccionar fecha" />
-												</div>
-											</div>
-											<Tabla />
-										</>
-									),
-								},
-								{
-									label: "Colapso",
-									key: "2",
-									children: (
-										<>
-											<div className="flex mb-4">
-												<div>
-													Fecha Inicio
-													<DatePicker className="ml-4" />
-												</div>
-												<div className="pl-4">
-													Fecha Fin
-													<DatePicker className="ml-4" />
-												</div>
-											</div>
-											<Tabla />
-										</>
-									),
-								},
-							]}
-						/>
-					</div>
+				<div className="mt-4">
+					<Tabs
+					defaultActiveKey="1"
+					className="w-full"
+					items={[
+						{
+						label: "Por fecha",
+						key: "1",
+						children: (
+							<>
+							<div className="flex mb-4">
+								<div>
+								Fecha Inicio
+								<DatePicker className="ml-4" placeholder="Seleccionar fecha" />
+								</div>
+								<div className="pl-4">
+								Fecha Fin
+								<DatePicker className="ml-4" placeholder="Seleccionar fecha" />
+								</div>
+							</div>
+							<TablaFlota />
+							</>
+						),
+						},
+						{
+						label: "Colapso",
+						key: "2",
+						children: (
+							<>
+							<div className="flex mb-4">
+								<div>
+								Fecha Inicio
+								<DatePicker className="ml-4" />
+								</div>
+								<div className="pl-4">
+								Fecha Fin
+								<DatePicker className="ml-4" />
+								</div>
+							</div>
+							<TablaFlota />
+							</>
+						),
+						},
+					]}
+					/>
+				</div>
 				)}
 			</div>
 		</div>
