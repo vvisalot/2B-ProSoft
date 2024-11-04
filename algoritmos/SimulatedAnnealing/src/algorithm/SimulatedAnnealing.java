c13a6af0dd0d9eb937192ca9cced9325ceaccf86
vvisalot <valeria.visalot@pucp.edu.pe>
2024-11-03 20:28:13 -0500

    (feat): Para enviar un json del resultado del SA al front

--- algoritmos/SimulatedAnnealing/src/algorithm/SimulatedAnnealing.java
+++ algoritmos/SimulatedAnnealing/src/algorithm/SimulatedAnnealing.java
@@ -1,17 +1,21 @@
 package algorithm;
 
+import dto.Solucion;
 import model.*;
 import utils.RelojSimulado;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class SimulatedAnnealing {
     private static final MapaVelocidad mapaVelocidad = MapaVelocidad.getInstance();
     private static final double TIEMPO_DESCARGA = 1;
+    private static final double TIEMPO_EN_OFICINA = 2;
     private final GrafoTramos grafoTramos = GrafoTramos.getInstance();
 
-    public static double calcular(List<Paquete> paquetes, Camion camion, RelojSimulado reloj,
+    public static Solucion calcular(List<Paquete> paquetes, Camion camion, RelojSimulado reloj,
                                   List<Oficina> almacenesPrincipales) {
         RutaManager.limpiarPaquetes();
         for (var paquete : paquetes) {
@@ -78,6 +82,7 @@ public class SimulatedAnnealing {
             }
             temp *= 1 - coolingRate;
         }
+        best.construirRutaMarcada();
         var posicionFinal = best.getPaquetesEntregados().get(best.getPaquetesEntregados().size() - 1).getVenta().getDestino();
         camion.setPosicionFinal(posicionFinal);
 
@@ -92,22 +97,88 @@ public class SimulatedAnnealing {
         //Escoger almacén de retorno
         var mejorTiempo = Double.MAX_VALUE;
         Oficina almacenRegreso = null;
+        Ruta rutaRegreso = new Ruta();
         for (Oficina almacen : almacenesPrincipales) {
-            var ruta = new Ruta(posicionFinal, almacen);
-            var tiempoRegreso = ruta.getTiempoTotal();
+            rutaRegreso = new Ruta(posicionFinal, almacen);
+            var tiempoRegreso = rutaRegreso.getTiempoRegreso();
             if (tiempoRegreso < mejorTiempo) {
                 mejorTiempo = tiempoRegreso;
                 almacenRegreso = almacen;
             }
         }
-        hoursToAdd = mejorTiempo;
+        System.out.println(rutaRegreso);
+
+
+
+        camion.setAlmacenCarga(almacenRegreso);
+        var paquetesAEntregar = new ArrayList<dto.Paquete>();
+
+        for(int i = 1; i < best.getPaquetesEntregados().size();i++){
+            var paquete = best.getPaquetesEntregados().get(i);
+            var destino = paquete.getVenta().getDestino();
+            paquetesAEntregar.add(new dto.Paquete(
+                    paquete.getVenta().getCodigo(),
+                    paquete.getVenta().getFechaHora(),
+                    new dto.Oficina(destino.getLatitud(),destino.getLongitud()),
+                    paquete.getCantidad(),
+                    paquete.getVenta().getCantidad(),
+                    paquete.getVenta().getIdCliente()
+            ));
+        }
+        var camionSolucion = new dto.Camion(camion.getCodigo(),camion.getTipo(),camion.getCapacidad(),camion.getCargaActual(),paquetesAEntregar);
+        var tramosSolucion = new ArrayList<dto.Tramo>();
+        LocalDateTime tiempoActual = reloj.getTiempo();
+        for (int i=0;  i< best.getRutaRecorrida().size(); i++) {
+            var tramo = best.getRutaRecorrida().get(i);
+            var origen = tramo.getOrigen();
+            var destino = tramo.getDestino();
+            var velocidad = mapaVelocidad.obtenerVelocidad(origen.getRegion(), destino.getRegion());
+            tramosSolucion.add(new dto.Tramo(
+                    new dto.Oficina(origen.getLatitud(),origen.getLongitud()),
+                    new dto.Oficina(destino.getLatitud(),destino.getLongitud()),
+                    tramo.getDistancia(),
+                    velocidad,
+                    tiempoActual,
+                    tiempoActual = tiempoActual.plusMinutes((long) (tramo.getDistancia() * 60 / velocidad)),
+                    i != best.getRutaRecorrida().size() -1 ? (int) TIEMPO_EN_OFICINA: (int) TIEMPO_DESCARGA,
+                    tramo.getEsFinal()
+            ));
+            tiempoActual = tiempoActual.plusHours((long) TIEMPO_EN_OFICINA);
+        }
+        best.desmarcarTramos();
+        for (int i=0;  i< rutaRegreso.getRutaRecorrida().size(); i++) {
+            var tramo = rutaRegreso.getRutaRecorrida().get(i);
+            var origen = tramo.getOrigen();
+            var destino = tramo.getDestino();
+            var velocidad = mapaVelocidad.obtenerVelocidad(origen.getRegion(), destino.getRegion());
+            tramosSolucion.add(new dto.Tramo(
+                    new dto.Oficina(origen.getLatitud(),origen.getLongitud()),
+                    new dto.Oficina(destino.getLatitud(),destino.getLongitud()),
+                    tramo.getDistancia(),
+                    velocidad,
+                    tiempoActual,
+                    tiempoActual = tiempoActual.plusMinutes((long) (tramo.getDistancia() * 60.0 / (double) velocidad)),
+                    (int) TIEMPO_EN_OFICINA,
+                    tramo.getEsFinal()
+            ));
+            tiempoActual = tiempoActual.plusHours((long) TIEMPO_EN_OFICINA);
+        }
+
+        hoursToAdd = bestTime + mejorTiempo;
         wholeHours = (long) hoursToAdd;
         minutes = (long) ((hoursToAdd - wholeHours) * 60);
-        camion.setRegresoAlmacen(fechaEntregaUltimoPaquete.plusHours(wholeHours).plusMinutes(minutes));
-        camion.setAlmacenCarga(almacenRegreso);
+        camion.setRegresoAlmacen(reloj.getTiempo().plusHours(wholeHours).plusMinutes(minutes));
+
+        var solucion = new Solucion(
+                camionSolucion,
+                tramosSolucion,
+                hoursToAdd
+        );
+
+
 //        System.out.println("Tiempo Final solución: " + best.getTiempoTotal());
         System.out.println("Ruta: " + best);
-        return bestTime + mejorTiempo;
+        return solucion;
     }
 
     public static double acceptanceProbability(double currentTime, double newTime, double temperature) {
@@ -135,33 +206,41 @@ public class SimulatedAnnealing {
             ruta.construirRutaMarcada();
             var rutaEvaluar = ruta.getRutaRecorrida();
             var tiempoEnRuta = 0.0;
-            for (Tramo tramo : rutaEvaluar) {
+            for (int i = 0; i< rutaEvaluar.size(); i++) {
+                var tramo = rutaEvaluar.get(i);
                 var velocidad = mapaVelocidad.obtenerVelocidad(tramo.getOrigen().getRegion(), tramo.getDestino().getRegion());
                 tiempoEnRuta += tramo.getDistancia() / velocidad;
-                tiempoEnRuta += TIEMPO_DESCARGA;
+                tiempoEnRuta += i != rutaEvaluar.size() -1 ? TIEMPO_EN_OFICINA: TIEMPO_DESCARGA;
                 if (tramo.getEsFinal() && tramo.getDestino().getRegion().equals("COSTA") && tiempoEnRuta >= 24.0) {
+                    ruta.desmarcarTramos();
                     return true;
                 }
             }
+            ruta.desmarcarTramos();
             return false;
         } else if (tiempo <= 72.0) {
             ruta.construirRutaMarcada();
             var rutaEvaluar = ruta.getRutaRecorrida();
             var tiempoEnRuta = 0.0;
-            for (Tramo tramo : rutaEvaluar) {
+            for (int i = 0; i< rutaEvaluar.size(); i++) {
+                var tramo = rutaEvaluar.get(i);
                 var velocidad = mapaVelocidad.obtenerVelocidad(tramo.getOrigen().getRegion(), tramo.getDestino().getRegion());
                 tiempoEnRuta += tramo.getDistancia() / velocidad;
+                tiempoEnRuta += i != rutaEvaluar.size() -1 ? TIEMPO_EN_OFICINA: TIEMPO_DESCARGA;
                 if (tramo.getEsFinal()) {
                     if (tramo.getDestino().getRegion().equals("COSTA") && tiempoEnRuta >= 24.0) {
+                        ruta.desmarcarTramos();
                         return true;
                     } else if (tramo.getDestino().getRegion().equals("SIERRA") && tiempoEnRuta >= 48.0) {
+                        ruta.desmarcarTramos();
                         return true;
                     }
                 }
             }
+            ruta.desmarcarTramos();
             return false;
         }
-        //TODO:Desmarcar las rutas
+        ruta.desmarcarTramos();
         return true;
     }
 }
---
+++ algoritmos/SimulatedAnnealing/src/dto/Paquete.java
@@ -0,0 +1,12 @@
+package dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record Paquete(UUID codigo,
+        LocalDateTime fechaHoraPedido,
+        Oficina destino,
+        int cantidadEntregada,
+        int cantidadTotal,
+        String idCliente) {
+}
